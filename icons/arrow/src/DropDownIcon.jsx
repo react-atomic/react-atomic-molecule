4bca2ea184c7ff188271c2544a35cf22ad699be0
Hill <hill@kimo.com>
2016-08-26 08:30:12 +0000

    update dropdown icon

--- icons/arrow/src/DropDownIcon.jsx
+++ icons/arrow/src/DropDownIcon.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const DropDownIcon = () => (
+const DropDownIcon = (props) => (
 <svg  {...props}>
-    <path d="M7 10l5 5 5-5z"/>
-    <path d="M0 0h24v24H0z" fill="none"/>
+    <path d="M7 10l5 5 5-5z" />
+    <path d="M0 0h24v24H0z" fill="none" />
 </svg>
 );
 
 DropDownIcon.defaultProps = {
     width: 24,
     height: 24,
-    viewBox: '0 0 160 160',
+    viewBox: '0 0 24 24',
 };
 
 export default DropDownIcon;
