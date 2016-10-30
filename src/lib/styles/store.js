bbe933aaffa8103b843a8dde2614ae6e20c80ceb
Hill <hill@kimo.com>
2016-10-30 15:28:13 +0000

    Make styles store to global

--- src/lib/styles/store.js
+++ src/lib/styles/store.js
@@ -1,6 +1,17 @@
-module.exports={
+
+let stylesStore = {
     styles:[],
     registry:[],
     newStyles:[],
     counter:0
 };
+
+if ('undefined' != typeof window) {
+    if (window.reactStylesStore) {
+        stylesStore = window.reactStylesStore;
+    } else {
+        window.reactStylesStore = stylesStore;
+    }
+}
+
+module.exports = stylesStore;
