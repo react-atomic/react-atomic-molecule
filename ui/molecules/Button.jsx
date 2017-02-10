b0fb21f58cc1a246035964edea22745ac1008e91
Hill <hill@kimo.com>
2017-02-10 13:20:17 +0000

    Improve button style

--- ui/molecules/Button.jsx
+++ ui/molecules/Button.jsx
@@ -7,21 +7,50 @@ import {
 
 const Button = (props) =>
 {
-    const {className, children, icon, ...others} = props;
+    const {className, children, icon, style, ...others} = props;
     const classes = mixClass (
         className,
         'button'
     );
     let thisIcon;
+    let buttonWithIconStyle;
     if (icon) {
-        thisIcon = <Icon>{icon}</Icon>;
+        thisIcon = <Icon style={Styles.icon}>{icon}</Icon>;
+        buttonWithIconStyle = Styles.buttonWithIcon; 
     }
     return (
-        <SemanticUI atom="button" {...others} className={classes}>
+        <SemanticUI
+            atom="button"
+            {...others}
+            className={classes}
+            style={{
+               ...buttonWithIconStyle,
+               ...style 
+            }}
+        >
             {thisIcon}
             {children}
         </SemanticUI>
     );
 }
 
+Button.defaultProps = {
+    type: 'button'
+};
+
 export default Button;
+
+const Styles = {
+    icon: {
+        width: 25,
+        height: 25,
+        left: 20,
+        top: 7,
+        fill: '#fff',
+        position: 'absolute'
+    },
+    buttonWithIcon: {
+        position: 'relative',
+        paddingLeft: 44 
+    }
+};
