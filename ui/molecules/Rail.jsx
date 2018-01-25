d18834fe92cca469fe4c66d3367bacaa58142058
Hill <hill@kimo.com>
2018-01-25 10:34:51 +0000

    add attached for rail

--- ui/molecules/Rail.jsx
+++ ui/molecules/Rail.jsx
@@ -5,12 +5,13 @@ import SemanticUI from '../molecules/SemanticUI';
 
 const Rail = (props) =>
 {
-    const {left, ...others} = props;
+    const {attached, left, ...others} = props;
     const classes = mixClass (
         props.className,
         'rail',
         {
-            left: left
+            left,
+            attached
         }
     );
     return (
@@ -20,6 +21,9 @@ const Rail = (props) =>
     );
 }
 
-Rail.defaultProps = {left: true};
+Rail.defaultProps = {
+    attached: true,
+    left: true
+};
 
 export default Rail;
