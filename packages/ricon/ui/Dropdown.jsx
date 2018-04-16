dbaf8d5a391d9907286942d2609be4bb34f5f066
Hill <hill@kimo.com>
2018-04-16 14:22:16 +0000

    Add dropdown icon (left, right)

--- packages/ricon/ui/Dropdown.jsx
+++ packages/ricon/ui/Dropdown.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Svg from './Svg';
 import Path from './Path';
 
-const ArrowPath = (props) => (
+const ArrowPath = props => (
     <Path
         d="M7 10l5 5 5-5z"
         {...props}
@@ -11,8 +11,16 @@ const ArrowPath = (props) => (
 
 const Dropdown = ({type, ...props}) => { 
     let transform = null;
-    if ('up'===type) {
-        transform = "rotate(180,12,13)";
+    switch (type) {
+        case 'up':
+            transform = "rotate(180,12,13)";
+            break;
+        case 'left':
+            transform = "rotate(90,12,13)";
+            break;
+        case 'right':
+            transform = "rotate(270,12,13)";
+            break;
     }
     return (
         <Svg {...props}>
