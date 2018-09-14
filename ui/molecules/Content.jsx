9b888934a1dc7cf7c9d3ebcc646c6d226cbdf6a0
Hill <hill@kimo.com>
2018-09-14 07:47:11 +0000

    fixed content boxsizing

--- ui/molecules/Content.jsx
+++ ui/molecules/Content.jsx
@@ -1,19 +1,16 @@
-import React from 'react'; 
+import React from 'react';
 import {mixClass} from 'class-lib';
 import SemanticUI from '../molecules/SemanticUI';
 
-const Content = (props)=>
-{
-    const classes = mixClass (
-        props.className,
-        'content'
-    );
-    return (
-      <SemanticUI {...props}
-        ui={false}
-        className={classes}
-      />
-    );
-}
+const Content = ({className, style, ...props}) => {
+  const classes = mixClass(className, 'content');
+  return (
+    <SemanticUI
+      ui={false}
+      {...props}
+      className={classes}
+      style={{...style, boxSizing: 'inherit'}}
+    />
+  );
+};
 export default Content;
-
