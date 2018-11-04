af1556af771154f18c03ad2140f06e5d60243120
Hill <hill@kimo.com>
2018-11-04 15:16:50 +0800

    fixed get not found

--- ui/molecules/Field.jsx
+++ ui/molecules/Field.jsx
@@ -1,121 +1,109 @@
 /* jshint esnext: true */
-import React, {cloneElement} from 'react'; 
+import React, {cloneElement} from 'react';
 import {mixClass} from 'class-lib';
+import get from 'get-object-value';
 import SemanticUI from '../molecules/SemanticUI';
 
-const Field = (props) => {
-    const {
-        className,
-        fieldClassName,
-        fieldStyle,
-        fieldStyles,
-        children,
-        inline,
-        type,
-        inputComponent,
-        inputWrapper,
-        inputWrapperAttr,
-        label,
-        labelStyle,
-        labelStyles,
-        style,
+const Field = props => {
+  const {
+    className,
+    fieldClassName,
+    fieldStyle,
+    fieldStyles,
+    children,
+    inline,
+    type,
+    inputComponent,
+    inputWrapper,
+    inputWrapperAttr,
+    label,
+    labelStyle,
+    labelStyles,
+    style,
+    styleOrder,
+    required,
+    ...others
+  } = props;
+  const isGroup = !(props.atom || inputComponent);
+  const classes = mixClass(fieldClassName, {
+    required: !!required,
+    field: !isGroup,
+    fields: isGroup,
+    inline: !!inline,
+  });
+  let oLabel = null;
+  if (label) {
+    let thisLabelStyle = get(labelStyle, null, {});
+    if (props.id) {
+      thisLabelStyle.cursor = 'pointer';
+    }
+    oLabel = (
+      <SemanticUI
+        atom="label"
+        key="label"
+        htmlFor={props.id}
+        style={thisLabelStyle}
+        styles={labelStyles}
+        styleOrder={styleOrder}>
+        {label}
+      </SemanticUI>
+    );
+  }
+  let input = null;
+  let thisFieldStyles = fieldStyles;
+  let thisFieldStyle = fieldStyle;
+  let thisChildren = children;
+  if (isGroup) {
+    thisFieldStyles = props.styles;
+    thisFieldStyle = props.style;
+  } else {
+    let inputChildren = null;
+    const isSelect = 'select' === props.atom;
+    const inputClasses = mixClass(className, {
+      dropdown: isSelect,
+    });
+    if (isSelect) {
+      thisChildren = null;
+      inputChildren = children;
+    }
+    input = inputComponent ? inputComponent : <SemanticUI />;
+    input = cloneElement(
+      input,
+      {
+        ...others,
+        style: {
+          boxSizing: 'inherit',
+          ...style,
+        },
+        key: 'input',
+        className: inputClasses,
         styleOrder,
         required,
-        ...others
-    } = props;
-    const isGroup = !(props.atom || inputComponent);
-    const classes = mixClass(
-        fieldClassName,
-        {
-            required: !!required,
-            field: !isGroup,
-            fields: isGroup,
-            inline: !!inline
-        }
+        type,
+      },
+      inputChildren,
     );
-    let oLabel = null;
-    if (label) {
-        let thisLabelStyle = get(labelStyle, null, {}); 
-        if (props.id) {
-            thisLabelStyle.cursor = 'pointer';
-        }
-        oLabel = 
-            <SemanticUI
-                atom="label"
-                key="label"
-                htmlFor={props.id}
-                style={thisLabelStyle}
-                styles={labelStyles}
-                styleOrder={styleOrder}
-            >
-                {label}
-            </SemanticUI>;
-    }
-    let input = null;
-    let thisFieldStyles = fieldStyles;
-    let thisFieldStyle = fieldStyle;
-    let thisChildren = children;
-    if (isGroup) {
-        thisFieldStyles = props.styles;
-        thisFieldStyle = props.style;
-    } else {
-        let inputChildren = null;
-        const isSelect = 'select' === props.atom;
-        const inputClasses = mixClass(
-            className,
-            {
-                'dropdown': isSelect
-            }
-        );
-        if (isSelect) {
-            thisChildren = null;
-            inputChildren = children;
-        }
-        input = (inputComponent) ? inputComponent : <SemanticUI />;
-        input = cloneElement(input, { 
-            ...others,
-            style: {
-                boxSizing: 'inherit',
-                ...style
-            },
-            key: 'input',
-            className: inputClasses,
-            styleOrder,
-            required,
-            type,
-        }, inputChildren);
-    }
-    let inputs;
-    if ('checkbox'===type || 'radio'===type) { 
-        inputs = [
-            input,
-            oLabel,
-        ];
-    } else {
-        inputs = [
-            oLabel,
-            input,
-        ];
-    }
-    if (inputWrapper) {
-        inputs = cloneElement(
-            inputWrapper,
-            inputWrapperAttr,
-            inputs
-        );
-    }
-    
-    return (
-        <SemanticUI
-            className={classes}
-            style={thisFieldStyle}
-            styles={thisFieldStyles}
-            styleOrder={styleOrder}
-        >
-            {inputs}
-            {thisChildren}
-        </SemanticUI>
-    );
+  }
+  let inputs;
+  if ('checkbox' === type || 'radio' === type) {
+    inputs = [input, oLabel];
+  } else {
+    inputs = [oLabel, input];
+  }
+  if (inputWrapper) {
+    inputs = cloneElement(inputWrapper, inputWrapperAttr, inputs);
+  }
+
+  return (
+    <SemanticUI
+      className={classes}
+      style={thisFieldStyle}
+      styles={thisFieldStyles}
+      styleOrder={styleOrder}>
+      {inputs}
+      {thisChildren}
+    </SemanticUI>
+  );
 };
 
 export default Field;
