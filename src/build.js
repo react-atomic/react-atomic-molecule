b6decb2cf0f2566ab7076080d400aff3869b9e06
Hill <hill@kimo.com>
2019-04-22 23:09:37 +0800

    fixed build, if child not defined

--- src/build.js
+++ src/build.js
@@ -1,6 +1,8 @@
-import {isValidElement, cloneElement, createElement} from 'react'; 
+import {isValidElement, cloneElement, createElement} from 'react';
 
 const build = component => (props, child) =>
-  (isValidElement(component) ? cloneElement : createElement)(component, props, child);
+  (isValidElement(component) ? cloneElement : createElement).apply(
+    null, child ? [component, props, child] : [component, props]
+  );
 
 export default build;
