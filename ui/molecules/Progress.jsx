659ff60c062f9daf139d9feda4c6a1539945ab2c
Hill <hill@kimo.com>
2019-11-15 14:57:04 +0800

    Add progress child support

--- ui/molecules/Progress.jsx
+++ ui/molecules/Progress.jsx
@@ -1,53 +1,54 @@
-import React from 'react'; 
-import reactStyle from '../../src/lib/styles/index'; 
+import React from 'react';
+import reactStyle from '../../src/lib/styles/index';
 import {mixClass} from 'class-lib';
 import SemanticUI from '../molecules/SemanticUI';
 
-const Progress = (props) =>
-{
-    const {
-        barProps,
-        className,
-        percent,
-        style,
-        styles,
-        ...others
-    } = props;
-    const {
-        style: barStyle,
-        styles: barStyles,
-        ...otherBarProps
-    } = barProps || {};
-    const classes = mixClass (
-        className,
-        'progress'
-    );
+const Progress = props => {
+  const {
+    barProps,
+    children,
+    className,
+    percent,
+    style,
+    styles,
+    ...others
+  } = props;
+  const {style: barStyle, styles: barStyles, ...otherBarProps} = barProps || {};
+  const classes = mixClass(className, 'progress');
 
-    return (
-        <SemanticUI
-            {...others}
-            className={classes}
-            styles={[
-                reactStyle({
-                    fontSize: 0,
-                    ...style
-                }, null, false),
-                styles
-            ]}
-        >
-            <SemanticUI
-                className="bar"
-                {...otherBarProps}
-                styles={[ 
-                    reactStyle({
-                        width: percent+'%',
-                        ...barStyle
-                    }, null, false),
-                    barStyles
-                ]}
-            />
-        </SemanticUI>
-    );
-}
+  return (
+    <SemanticUI
+      {...others}
+      className={classes}
+      styles={[
+        reactStyle(
+          {
+            fontSize: 0,
+            ...style,
+          },
+          null,
+          false,
+        ),
+        styles,
+      ]}>
+      <SemanticUI
+        className="bar"
+        {...otherBarProps}
+        styles={[
+          reactStyle(
+            {
+              width: percent + '%',
+              ...barStyle,
+            },
+            null,
+            false,
+          ),
+          barStyles,
+        ]}
+      />
+      {children}
+    </SemanticUI>
+  );
+};
 
 export default Progress;
