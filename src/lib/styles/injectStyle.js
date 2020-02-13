fea39547580261229d95bd786b1e038ccf90a2bb
Hill <hill@kimo.com>
2020-02-13 20:12:31 +0800

    new react-style for create or update dom with id

--- src/lib/styles/injectStyle.js
+++ src/lib/styles/injectStyle.js
@@ -1,37 +1,51 @@
 import {doc} from 'win-doc';
 import store from './store.js';
 import stylesToCSS from './stylesToCSS';
+import {inject, create} from 'create-el';
+import query from 'css-query-selector';
 
 const reInjectStyle = () => {
-    store.newStyles = Object.values(store.registry);
-    injectStyle();
+  store.newStyles = Object.values(store.registry);
+  injectStyle();
 };
 
-const appendCss = css => {
-    const oDoc = doc();
-    const tag = oDoc.createElement('style');
-    tag.innerHTML = css;
-    oDoc.getElementsByTagName('head')[0].appendChild(tag);
+const keys = Object.keys;
+
+const appendCss = cssArr => {
+  keys(cssArr).forEach(key => {
+    const id = 'react-style-'+key;
+    let styleDom = query.one('#' + id);
+    const css = cssArr[key];
+    if (styleDom) {
+      styleDom.innerHTML = css;
+    } else {
+      styleDom = create('style')()({
+        id,
+        innerHTML: css,
+      });
+      inject(() => doc().getElementsByTagName('head')[0])(styleDom);
+    }
+  });
 };
 
 const injectStyle = () => {
-    if (!store.newStyles.length){
-      // We are in Node or Styles are already injected
-      return null;
+  if (!store.newStyles.length) {
+    // We are in Node or Styles are already injected
+    return null;
+  }
+  const compiled = stylesToCSS(store.newStyles);
+  store.newStyles = [];
+  store.registry = {
+    ...store.registry,
+    ...compiled.styleIds,
+  };
+  if (compiled.css) {
+    if (doc().URL) {
+      appendCss(compiled.cssArr);
+    } else {
+      console.log(compiled.css);
     }
-    const compiled = stylesToCSS(store.newStyles);
-    store.newStyles = [];
-    store.registry = { 
-        ...store.registry,
-        ...compiled.styleIds
-    };
-    if(compiled.css){
-        if (doc().URL) {
-            appendCss(compiled.css);
-        } else {
-            console.log(compiled.css);
-        }
-    }
+  }
 };
 
 export {reInjectStyle};
--- src/lib/styles/stylesToCSS.js
+++ src/lib/styles/stylesToCSS.js
@@ -1,16 +1,16 @@
-'use strict';
 import hyphenateStyleName from 'hyphenate-style-name';
+import get from 'get-object-value';
+
 
 // Follows syntax at https://developer.mozilla.org/en-US/docs/Web/CSS/content,
 // including multiple space separated values.
 const unquotedContentValueRegex = /^(normal|none|(\b(url\([^)]*\)|chapter_counter|attr\([^)]*\)|(no-)?(open|close)-quote|inherit)((\b\s*)|$|\s+))+)$/;
 
 const isArray = Array.isArray;
-const keys    = Object.keys;
-const browsers = ['webkit','moz'];
+const keys = Object.keys;
+const browsers = ['webkit', 'moz'];
 
-const buildRule = (key, value) =>
-{
+const buildRule = (key, value) => {
   if (null === value) {
     return '';
   }
@@ -19,91 +19,88 @@ const buildRule = (key, value) =>
     value = "'" + value.replace(/'/g, "\\'") + "'";
   }
   return hyphenateStyleName(key) + ': ' + value + ';';
-}
+};
 
-const buildRules = (result, rules, selector) =>
-{
+const buildRules = (result, styleId, selector) => {
+  const rules = get(result.styleIds, [styleId, 'style'], []); 
   if (!rules.length) {
     return result;
   }
   let mycss = '';
-  let parentSelector = '';
+  let parentSelector;
   if (isArray(selector)) {
-       parentSelector = selector[0].trim(); 
-       selector.shift();
+    parentSelector = selector[0].trim();
+    selector.shift();
   } else {
-       selector = [selector];
+    selector = [selector];
   }
 
-  rules.forEach((rule, i)=>{
-      mycss += selector[i] + ' {\n';
-      keys(rule).forEach( styleKey => {
-          if (rule[styleKey] && rule[styleKey].forEach) {
-            rule[styleKey].forEach((item)=>
-                mycss += buildRule(styleKey, item)
-            );
-          } else {
-            mycss += buildRule(styleKey, rule[styleKey]);
-          }
-      });
-      mycss += '}\n\n';
+  rules.forEach((rule, i) => {
+    mycss += selector[i] + ' {\n';
+    keys(rule).forEach(styleKey => {
+      if (rule[styleKey] && rule[styleKey].forEach) {
+        rule[styleKey].forEach(item => (mycss += buildRule(styleKey, item)));
+      } else {
+        mycss += buildRule(styleKey, rule[styleKey]);
+      }
+    });
+    mycss += '}\n\n';
   });
 
   if (parentSelector) {
-      const keyframesString = '@keyframes';
-      if (0===parentSelector.indexOf(keyframesString)) {
-        browsers.forEach((browser)=>{
-            result.css += parentSelector.replace(
-                    keyframesString,
-                    '@-'+browser+'-keyframes'
-            ) + ' {\n'+ mycss+ '}\n';
-        });
-      }
-      result.css += parentSelector+ ' {\n'+ mycss+ '}\n';
-  } else {
-      result.css += mycss;
+    const keyframesString = '@keyframes';
+    if (0 === parentSelector.indexOf(keyframesString)) {
+      browsers.forEach(browser => {
+        mycss +=
+          parentSelector.replace(
+            keyframesString,
+            '@-' + browser + '-keyframes',
+          ) +
+          ' {\n' +
+          mycss +
+          '}\n';
+      });
+    }
+    mycss += parentSelector + ' {\n' + mycss + '}\n';
   }
+  result.cssArr[styleId] = mycss;
+  result.css += mycss;
   return result;
-}
+};
 
-const replicateSelector = s =>
-{
-    s = '.'+s;
-    const a=[s];
-    for (let i=1; i < 10; i++ ) {
-        a[i] = a[(i-1)] + s + i;
-    }
-    return a.join(',');
-}
+const replicateSelector = s => {
+  s = '.' + s;
+  const a = [s];
+  for (let i = 1; i < 10; i++) {
+    a[i] = a[i - 1] + s + i;
+  }
+  return a.join(',');
+};
 
-const buildStyle = (result, oStyle) =>
-{
-    const styleId = oStyle.styleId;
-    if (!styleId || result.styleIds[styleId]) {
-        return;
+const buildStyle = (result, oStyle) => {
+  const {styleId} = oStyle;
+  if (!styleId || result.styleIds[styleId]) {
+    return;
+  }
+  let selector = oStyle.selector;
+  if (selector) {
+    if (isArray(selector) && !selector[1]) {
+      selector[1] = replicateSelector(styleId);
     }
-    let selector = oStyle.selector;
-    if (selector) {
-        if (isArray(selector) && !selector[1]) {
-            selector[1] = replicateSelector(styleId);
-        }
-    } else {
-        selector = replicateSelector(styleId);
-    }
-    result.styleIds[styleId] = oStyle; //for check already inject
-    buildRules(result, oStyle.style, selector);
-}
+  } else {
+    selector = replicateSelector(styleId);
+  }
+  result.styleIds[styleId] = oStyle; //for check already inject
+  buildRules(result, styleId, selector);
+};
 
-const stylesToCSS = styles =>
-{
-    if (!isArray(styles)) {
-        styles = [styles];
-    }
-    const result = {css: '', styleIds: {}};
-    styles.forEach((style)=>{
-        buildStyle(result, style);
-    });
-    return result;
-}
+const stylesToCSS = styles => {
+  if (!isArray(styles)) {
+    styles = [styles];
+  }
+  const result = {css: '', styleIds: {}, cssArr: {}};
+  styles.forEach(style => buildStyle(result, style));
+  return result;
+};
 
 export default stylesToCSS;
