8e34860da153c57725164629332e0c4cea790b3e
Hill <hill@kimo.com>
2020-09-21 01:16:50 +0800

    Add ViewModule icon

---
+++ packages/ricon/ui/ViewModule.jsx
@@ -0,0 +1,9 @@
+import base from "./base";
+
+const ViewModule = (props) => {
+  const d =
+    "M4 11h5V5H4v6zm0 7h5v-6H4v6zm6 0h5v-6h-5v6zm6 0h5v-6h-5v6zm-6-7h5V5h-5v6zm6-6v6h5V5h-5z";
+  return base("ViewModule")(d)(props);
+};
+
+export default ViewModule;
