e8ba41e10a619c869a795891546f518bbb57bf1f
Hill <hill@kimo.com>
2020-09-21 01:44:33 +0800

    Add callback support

--- packages/keyframe-css/src/index.js
+++ packages/keyframe-css/src/index.js
@@ -1,69 +1,71 @@
-'use strict';
-import {getDefault} from 'get-object-value';
-import {reactStyle, injectStyle} from 'react-atomic-molecule';
+import { getDefault } from "get-object-value";
+import { reactStyle, injectStyle } from "react-atomic-molecule";
+import callfunc from "call-func";
 
 const inject = {};
 let c = 0;
 
-const processCss = css => {
+const processCss = (cb) => (css) => {
   css = getDefault(css);
   const keys = Object.keys(css);
   if (keys.length) {
-    keys.forEach(key => {
-      css[key].push('keyframe-' + key + '-' + c);
+    keys.forEach((key) => {
+      css[key].push("keyframe-" + key + "-" + c);
       reactStyle.apply(null, css[key]);
       c++;
     });
     injectStyle();
   }
+  callfunc(cb);
 };
 
-const getKeyframeCss = key => {
+const getKeyframeCss = (key, cb) => {
   if (inject[key]) {
     return;
   }
+  const callback = processCss(cb);
   switch (key) {
-    case 'candleInTheWind':
-      import('./candleInTheWind').then(css => processCss(css));
+    case "candleInTheWind":
+      import("./candleInTheWind").then(callback);
       break;
-    case 'fadeIn':
-      import('./fadeIn').then(css => processCss(css));
+    case "fadeIn":
+      import("./fadeIn").then(callback);
       break;
-    case 'fadeInUp':
-      import('./fadeInUp').then(css => processCss(css));
+    case "fadeInUp":
+      import("./fadeInUp").then(callback);
       break;
-    case 'fadeInUpBig':
-      import('./fadeInUpBig').then(css => processCss(css));
+    case "fadeInUpBig":
+      import("./fadeInUpBig").then(callback);
       break;
-    case 'fadeInRight':
-      import('./fadeInRight').then(css => processCss(css));
+    case "fadeInRight":
+      import("./fadeInRight").then(callback);
       break;
-    case 'fadeInDown':
-      import('./fadeInDown').then(css => processCss(css));
+    case "fadeInDown":
+      import("./fadeInDown").then(callback);
       break;
-    case 'fadeInLeft':
-      import('./fadeInLeft').then(css => processCss(css));
+    case "fadeInLeft":
+      import("./fadeInLeft").then(callback);
       break;
-    case 'fadeOut':
-      import('./fadeOut').then(css => processCss(css));
+    case "fadeOut":
+      import("./fadeOut").then(callback);
       break;
-    case 'fadeOutUp':
-      import('./fadeOutUp').then(css => processCss(css));
+    case "fadeOutUp":
+      import("./fadeOutUp").then(callback);
       break;
-    case 'fadeOutRight':
-      import('./fadeOutRight').then(css => processCss(css));
+    case "fadeOutRight":
+      import("./fadeOutRight").then(callback);
       break;
-    case 'fadeOutDown':
-      import('./fadeOutDown').then(css => processCss(css));
+    case "fadeOutDown":
+      import("./fadeOutDown").then(callback);
       break;
-    case 'fadeOutLeft':
-      import('./fadeOutLeft').then(css => processCss(css));
+    case "fadeOutLeft":
+      import("./fadeOutLeft").then(callback);
       break;
-    case 'pulsate':
-      import('./pulsate').then(css => processCss(css));
+    case "pulsate":
+      import("./pulsate").then(callback);
       break;
-    case 'spin':
-      import('./spin').then(css => processCss(css));
+    case "spin":
+      import("./spin").then(callback);
       break;
   }
   inject[key] = true;
