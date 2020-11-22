52e663d3cbbb76cb8bcd4a9f8ad93058c67bcf15
Hill <hill@kimo.com>
2020-11-22 14:19:25 +0800

    Add dropdown icon could calculable

--- packages/ricon/ui/Dropdown.jsx
+++ packages/ricon/ui/Dropdown.jsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import Svg from './Svg';
-import Path from './Path';
+import React from "react";
+import Svg from "./Svg";
+import Path from "./Path";
 
-const ArrowPath = props => (
-    <Path
-        d="M7 10l5 5 5-5z"
-        {...props}
-    />
+const ArrowPath = ({ size = 5, ...props }) => (
+  <Path
+    d={`M${Math.ceil((24 - size * 2) / 2)} ${Math.ceil((24 - size) / 2)}l${size} ${size} ${size}-${size}z`}
+    {...props}
+  />
 );
 
-const Dropdown = ({type, ...props}) => { 
-    let transform = null;
-    switch (type) {
-        case 'up':
-            transform = "rotate(180,12,13)";
-            break;
-        case 'left':
-            transform = "rotate(90,12,13)";
-            break;
-        case 'right':
-            transform = "rotate(270,12,13)";
-            break;
-    }
-    return (
-        <Svg {...props}>
-            <ArrowPath transform={transform} />
-        </Svg>
-    );
+const Dropdown = ({ type, ...props }) => {
+  let transform = null;
+  switch (type) {
+    case "up":
+      transform = "rotate(180,12,13)";
+      break;
+    case "left":
+      transform = "rotate(90,12,13)";
+      break;
+    case "right":
+      transform = "rotate(270,12,13)";
+      break;
+  }
+  return (
+    <Svg {...props}>
+      <ArrowPath transform={transform} />
+    </Svg>
+  );
 };
 
-export {ArrowPath};
+export { ArrowPath };
 export default Dropdown;
