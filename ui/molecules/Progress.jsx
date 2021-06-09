769803724decafadea093eac35409297112aedc5
Hill <hill@kimo.com>
2021-06-09 18:09:50 +0800

    add label and barLabel props

--- ui/molecules/Progress.jsx
+++ ui/molecules/Progress.jsx
@@ -2,16 +2,32 @@ import React from "react";
 import reactStyle from "../../src/lib/styles/index";
 import { mixClass } from "class-lib";
 import SemanticUI from "../molecules/SemanticUI";
+import Label from "../molecules/Label";
 
 const Progress = (props) => {
-  const { barProps, children, className, percent, style, styles, ...others } =
-    props;
+  const {
+    label,
+    barLabel,
+    barProps,
+    children,
+    className,
+    percent,
+    style,
+    styles,
+    ...others
+  } = props;
+  const thisLabel = label ? <Label>{label}</Label> : children;
   const {
     style: barStyle,
     styles: barStyles,
     ...otherBarProps
   } = barProps || {};
   const classes = mixClass(className, "progress");
+  otherBarProps.children = barLabel ? (
+    <Label>{barLabel}</Label>
+  ) : (
+    otherBarProps.children
+  );
 
   return (
     <SemanticUI
@@ -34,7 +50,7 @@ const Progress = (props) => {
           barStyles,
         ]}
       />
-      {children}
+      {thisLabel}
     </SemanticUI>
   );
 };
