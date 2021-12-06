a940cd284fc7ffac76157c792f2dcd4ba0afd74d
Hill <hill@kimo.com>
2021-12-07 01:17:40 +0800

    improve useLazyInject

--- src/lib/styles/useLazyInject.js
+++ src/lib/styles/useLazyInject.js
@@ -1,10 +1,13 @@
 import { useEffect, useRef } from "react";
 import lazyInject from "./lazyInject";
 
-const useLazyInject = (InjectStyles) => {
+const useLazyInject = (InjectStyles, injectStore) => {
   const injects = useRef();
   if (!injects.current) {
-    injects.current = lazyInject(injects.current, InjectStyles);
+    if (!injectStore) {
+      injectStore = lazyInject(injects.current, InjectStyles);
+    }
+    injects.current = injectStore;
   }
   return injects.current;
 };
