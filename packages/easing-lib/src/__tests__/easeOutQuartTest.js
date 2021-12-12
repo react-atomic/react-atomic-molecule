ed20d89251384e666dc1484b79759f16babb36bd
Hill <hill@kimo.com>
2021-12-12 08:52:29 +0800

    Add reverse test

--- packages/easing-lib/src/__tests__/easeOutQuartTest.js
+++ packages/easing-lib/src/__tests__/easeOutQuartTest.js
@@ -14,4 +14,16 @@ describe('Test easeOutQuart', () => {
     const actual5 = Math.floor(aniCal(5, start, to, duration));
     expect({actual1: 59, actual2: 87, actual3: 97, actual4: 99, actual5: 100}).to.deep.equal({actual1, actual2, actual3, actual4, actual5});
   });
+
+  it("test reduce", ()=>{
+    const start = 100; 
+    const to = 0;
+    const duration = 5;
+    const actual1 = start - Math.floor(aniCal(1, to, start, duration));
+    const actual2 = start - Math.floor(aniCal(2, to, start, duration));
+    const actual3 = start - Math.floor(aniCal(3, to, start, duration));
+    const actual4 = start - Math.floor(aniCal(4, to, start, duration));
+    const actual5 = start - Math.floor(aniCal(5, to, start, duration));
+    expect({actual1: 41, actual2: 13, actual3: 3, actual4: 1, actual5: 0}).to.deep.equal({actual1, actual2, actual3, actual4, actual5});
+  });
 });
