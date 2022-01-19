f5e7c3cb02f02d01d624375003b3e53a3dc1b1fe
Hill <hill@kimo.com>
2022-01-19 09:03:47 +0800

    improve SemanticUI

--- ui/molecules/SemanticUI.jsx
+++ ui/molecules/SemanticUI.jsx
@@ -2,24 +2,22 @@ import React, { createElement } from "react";
 import * as Atoms from "react-atomic-atom";
 import get from "get-object-value";
 import ucfirst from "ucfirst-js";
+import { KEYS } from "reshow-constant";
 
 import injectStyle from "../../src/lib/styles/injectStyle";
 import bindStyles from "../../src/lib/styles/bindStyles";
 import { bindChildKey } from "../../src/getChildMapping";
-import { mixClass } from "class-lib";
-
-const keys = Object.keys;
 
 const getChildren = (render, children) =>
   render ? bindChildKey(children) : null;
 
 const SemanticUI = ({
+  ui = true,
+  renderChildren = true,
   atom,
   children,
-  renderChildren,
   styles,
   styleOrder,
-  ui,
   ...others
 }) => {
   let component;
@@ -43,34 +41,28 @@ const SemanticUI = ({
       component = get(Atoms, [ucfirst(atom)], () => Atoms.Div);
       break;
   }
-  const { className, style } = others;
   // bindStyles need after inject
-  let bindProps = {};
   if (styles) {
     // Need avoid props pass by ref !!important!!
     injectStyle();
-    bindProps = bindStyles({
+    const { className, style } = others;
+    const bindProps = bindStyles({
       className,
       style,
       styles,
       styleOrder,
     });
+    KEYS(bindProps).forEach((key) => (others[key] = bindProps[key]));
   }
-  keys(bindProps).forEach((key) => (others[key] = bindProps[key]));
   if (ui) {
     // others.className maybe effect by bindProps, so use it here.
-    others.className = mixClass(others.className, "ui");
+    others.className += " ui";
   }
   return createElement(
     component,
     others,
     getChildren(renderChildren, children)
   );
-};
-
-SemanticUI.defaultProps = {
-  ui: true,
-  renderChildren: true,
 };
 
 export default SemanticUI;
