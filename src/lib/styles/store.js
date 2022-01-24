bff035bbbac986f9f6650e5de1fdba3e730b83aa
Hill <hill@kimo.com>
2022-01-25 05:26:27 +0800

    fixed npm link case

--- src/lib/styles/store.js
+++ src/lib/styles/store.js
@@ -3,12 +3,17 @@ import { win } from "win-doc";
 const oWin = win();
 const g = oWin ? oWin : global;
 
-const stylesStore = {
-  load: {},
-  registry: {},
-  newStyles: [],
-  counter: 0,
-};
+/**
+ * Assign value from g.reactStylesStore for sync npm link case
+ */
+const stylesStore = g.reactStylesStore
+  ? g.reactStylesStore
+  : {
+      load: {},
+      registry: {},
+      newStyles: [],
+      counter: 0,
+    };
 
 g.reactStylesStore = stylesStore;
 
