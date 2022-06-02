0a667a195d835a502669262029c7548eda8f35e1
Hill <hill@kimo.com>
2022-06-03 02:51:06 +0800

    Add zero test

--- src/lib/styles/__tests__/cssNumberToUnitTest.js
+++ src/lib/styles/__tests__/cssNumberToUnitTest.js
@@ -8,6 +8,11 @@ describe("Test cssNumberToUnit", () => {
     expect(actual).to.equal("1px");
   });
 
+  it("test zero", () => {
+    const actual = cssNumberToUnit("width", 0);
+    expect(actual).to.equal(0);
+  });
+
   it("test value with array", () => {
     const actual = cssNumberToUnit("width", [1, 1]);
     expect(actual).to.deep.equal(["1px", "1px"]);
