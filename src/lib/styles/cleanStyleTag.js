7f5e319be0b7231e9ae62d71392c13acc909fc51
Hill <hill@kimo.com>
2022-06-26 21:52:31 +0800

    fixed clean empty

--- src/lib/styles/cleanStyleTag.js
+++ src/lib/styles/cleanStyleTag.js
@@ -5,7 +5,7 @@ import query from "css-query-selector";
 import getStyleTagId from "./getStyleTagId";
 
 const cleanStyleTag = (StyleMap) => {
-  KEYS(StyleMap).forEach((key) => {
+  KEYS(StyleMap || []).forEach((key) => {
     const id = getStyleTagId(StyleMap[key].styleId);
     const styleDom = query.one("#" + id);
     remove(styleDom);
