889cd3a183c8cf46dc05116a78d68f489eb8203d
Hill <hill@kimo.com>
2022-07-22 22:28:19 +0800

    Redesing react atom for next hybrid version

---
+++ refCB.md
@@ -0,0 +1,49 @@
+## Fantastic Tip
+### refCB help get rid of findDOMNode
+Before:
+```JS
+const Input = (props) =>
+{
+    return <input />;
+}
+
+class MyInput extends Component 
+{
+  componentDidMount()
+  {
+      findDOMNode(this.el).focus();
+  } 
+
+  render()
+  {
+      return <Input ref={el=>this.el=el} />;
+  }
+}
+```
+
+After:
+```JS
+const Input = (props) =>
+{
+    let {refCb, ...others} = props;
+    if (refCb) {
+        others.ref = refCb;
+    }
+    return <input {...others} />;
+}
+
+class MyInput extends Component 
+{
+  componentDidMount()
+  {
+     this.el.focus();
+  } 
+
+  render()
+  {
+      return <Input refCb={el=>this.el=el} />;
+  }
+}
+```
+   * More refs https://facebook.github.io/react/docs/refs-and-the-dom.html
+
--- ui/molecules/SemanticUI.jsx
+++ ui/molecules/SemanticUI.jsx
@@ -1,8 +1,7 @@
 import { createElement } from "react";
-import * as Atoms from "react-atomic-atom";
+import { html } from "react-atomic-atom";
 import get from "get-object-value";
-import ucfirst from "ucfirst-js";
-import { KEYS } from "reshow-constant";
+import { KEYS, HAS } from "reshow-constant";
 
 import injectStyle from "../../src/lib/styles/injectStyle";
 import bindStyles from "../../src/lib/styles/bindStyles";
@@ -14,58 +13,56 @@ const getChildren = (render, children) =>
 const SemanticUI = ({
   ui = true,
   renderChildren = true,
+  refCb,
   atom,
   children,
   styles,
   styleOrder,
-  ...others
+  ...restProps
 }) => {
-  let component;
+  const atoms = html;
+  atom = null != atom ? atom : atoms.default;
   switch (atom) {
     case "null":
       return null;
-    case "input":
-      component = Atoms.Input;
-      renderChildren = false;
-      break;
-    case "img":
-      component = Atoms.Img;
-      renderChildren = false;
-      break;
-    case "path":
-      component = Atoms.Path;
-      renderChildren = false;
-      ui = false;
-      break;
     default:
-      component = get(Atoms, [ucfirst(atom)], () => Atoms.Div);
+      const componentData = get(atoms, [atom]);
+      if (componentData) {
+        if (HAS(componentData, "renderChildren")) {
+          renderChildren = componentData.renderChildren;
+        }
+        if (HAS(componentData, "ui")) {
+          renderChildren = componentData.ui;
+        }
+      }
       break;
   }
-  // bindStyles need after inject
   if (styles) {
-    const { className, style } = others;
+    const { className, style } = restProps;
     // Need avoid props pass by ref !!important!!
     injectStyle();
+    // bindStyles need after inject
     const bindProps = bindStyles({
       className,
       style,
       styles,
       styleOrder,
     });
-    KEYS(bindProps).forEach((key) => (others[key] = bindProps[key]));
+    KEYS(bindProps).forEach((key) => (restProps[key] = bindProps[key]));
   }
   if (ui) {
     /**
      * Need put after bindProps
-     *!!important!! others.className maybe effect by bindProps, so use it here.
+     *!!important!! restProps.className maybe effect by bindProps, so use it here.
      */
-    others.className = others.className ? others.className + " ui" : "ui";
+    restProps.className = restProps.className
+      ? restProps.className + " ui"
+      : "ui";
   }
-  return createElement(
-    component,
-    others,
-    getChildren(renderChildren, children)
-  );
+  if (refCb) {
+    restProps.ref = refCb;
+  }
+  return createElement(atom, restProps, getChildren(renderChildren, children));
 };
 
 export default SemanticUI;
