235962bf4f75881e0c7753a3032ae1a4b7c58ef0
Hill <hill@kimo.com>
2022-08-17 00:23:19 +0800

    fixed zoomin

--- packages/ricon/ui/base.jsx
+++ packages/ricon/ui/base.jsx
@@ -2,12 +2,15 @@ import Svg from "./Svg";
 import Path from "./Path";
 
 const base = (displayName, d) => {
-  const Element = ({ type = null, children, ...props }) => (
-    <Svg {...props} data-name={displayName}>
-      <Path d={d[type] || d["_"] || d} />
-      {children}
-    </Svg>
-  );
+  const Element = ({ type = null, children, ...props }) => {
+    const path = d[type] || d["_"] || d;
+    return (
+      <Svg {...props} data-name={displayName}>
+        {path.map ? path.map((p) => <Path d={p} />) : <Path d={path} />}
+        {children}
+      </Svg>
+    );
+  };
   Element.displayName = displayName;
   return Element;
 };
